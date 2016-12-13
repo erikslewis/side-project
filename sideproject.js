8d5b4b841df48037c7c1a5064d16f21712bf9400
erikslewis <Adelina198527@gmail.com>
2016-12-13 18:33:55 -0500

    PT

---
+++ sideproject.js
@@ -0,0 +1,45 @@
+$(function(){
+    $("#show").hide();
+    $("#button1").on("click", function(){
+        $("#hide, #show").toggle();
+    });
+  });
+
+$(function(){
+    $("#show").hide();
+    $("#button2").on("click", function(){
+    $("#hide, #show").toggle();
+    });
+  });
+
+$(function(){
+    $("#show").hide();
+    $("#button3").on("click", function(){
+    $("#hide, #show").toggle();
+    });
+});
+
+    $(function(){
+            $("#show").hide();
+            $("#button4").on("click", function(){
+                $("#hide, #show").toggle();
+            });
+            });
+            $(function(){
+                $("#show").hide();
+                $("#button5").on("click", function(){
+                $("#hide, #show").toggle();
+                });
+               });
+                $(function(){
+                    $("#show").hide();
+                    $("#button6").on("click", function(){
+                    $("#hide, #show").toggle();
+                    });
+                  });
+                    $(function(){
+                        $("#show").hide();
+                        $("#button7").on("click", function(){
+                        $("#hide, #show").toggle();
+                        });
+                      });
