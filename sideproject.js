413a8ad75712029e65e134702e754f4e76f539e3
erikslewis <Adelina198527@gmail.com>
2016-12-19 13:25:31 -0500

    timer added

--- sideproject.js
+++ sideproject.js
@@ -1,3 +1,48 @@
+$( document ).ready(function(){
+var timer = {
+  seconds: 0,
+  timerId: 0,
+  run: false,
+
+  clock: {
+    timer: $("#timer"),
+    reset: $("#reset"),
+    start: $("#start"),
+    pause: $("#pause"),
+  },
+
+  updateTime: function updateTime(){
+    this.seconds++;
+    this.clock.timer.text('Time elapsed: ' + this.seconds);
+  },
+
+  setupListeners: function () {
+    this.clock.start.on('click', function() {
+      console.log('start');
+      this.clock.timer.text('Time elapsed: ' + this.seconds);
+      if (this.run === false){
+        this.timerId = setInterval(this.updateTime.bind(this), 1000);
+        this.run = true;
+      }
+    }.bind(this));
+
+    this.clock.pause.on('click', function() {
+      clearInterval(this.timerId);
+      this.run = false;
+    }.bind(this));
+
+    this.clock.reset.on('click', function() {
+      this.seconds = 0;
+      console.log('reset ' + this.seconds);
+      clearInterval(this.timerId);
+      this.run = false;
+      this.clock.timer.text('Stop Watch');
+    }.bind(this));
+  }
+}
+
+timer.setupListeners();
+
 $(function(){
     $("#show1").hide();
     $("#button1").on("click", function(){
@@ -44,3 +89,4 @@ $(function(){
 
                         });
                       });
+                    });
