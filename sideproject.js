5351e4dca99623323dfdb3af1d8d66c226028314
erikslewis <Adelina198527@gmail.com>
2016-12-19 08:35:38 -0500

    extra

--- sideproject.js
+++ sideproject.js
@@ -1,45 +1,46 @@
 $(function(){
     $("#show1").hide();
     $("#button1").on("click", function(){
-        $("#hide, #show1").toggle();
+        $("#hide, #show1").toggle(450);
     });
   });
 
 $(function(){
     $("#show2").hide();
     $("#button2").on("click", function(){
-    $("#hide, #show2").toggle();
+    $("#hide, #show2").toggle(450);
     });
   });
 
 $(function(){
     $("#show3").hide();
     $("#button3").on("click", function(){
-    $("#hide, #show3").toggle();
+    $("#hide, #show3").toggle(450);
     });
 });
 
 $(function(){
             $("#show4").hide();
             $("#button4").on("click", function(){
-                $("#hide, #show4").toggle();
+                $("#hide, #show4").toggle(450);
             });
             });
             $(function(){
                 $("#show5").hide();
                 $("#button5").on("click", function(){
-                $("#hide, #show5").toggle();
+                $("#hide, #show5").toggle(450);
                 });
                });
                 $(function(){
                     $("#show6").hide();
                     $("#button6").on("click", function(){
-                    $("#hide, #show6").toggle();
+                    $("#hide, #show6").toggle(450);
                     });
                   });
                     $(function(){
                         $("#show7").hide();
                         $("#button7").on("click", function(){
-                        $("#hide, #show7").toggle();
+                        $("#hide, #show7").toggle(450);
+
                         });
                       });
